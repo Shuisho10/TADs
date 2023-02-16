76121478a1905889ede5d9843ce1bbb12b418db9
Shuisho10 <54242777+Shuisho10@users.noreply.github.com>
2023-02-16 23:52:24 +0100

    Observadores, modificadores y destructor
    Implementado todas las funciones observadores, modificadores y destructor que tenia almacenado y actualizado el orden de las inicializaciones en el constructor para evitar errores.

--- cola_dinamica.h
+++ cola_dinamica.h
@@ -6,25 +6,75 @@ template <typename T>
 class ColaD
 {
 public:
-    explicit ColaD(size_t s);
-    void vacia();
-    const T& tope() const;
-    void push();
-    T pop();
-    ~ColaD();
+    explicit ColaD(int s);       //Constructor por defecto
+    //TODO COPIA
+    bool vacia() const;             //Observador de cola vacia
+    bool llena() const;             //Observador de cola llena
+    const T& tope() const;          //Observador del siguiente elemento en la cola
+
+    void push(const T& in);         //Modificador de addición de elemento a la cola
+    T pop();                        //Modificador de sacar elemento de la cola
+    //TODO OPERADOR ASIGNACION
+    ~ColaD();                       //Destructor
 private:
+    int lmax_;
+    int i_, n_;
     T *elementos_;
-    size_t lmax_;
-    size_t i_, n_;
+
 };
 
+//Constructor (REQUIERE CONSTRUCTOR DE T)
 template <typename T>
-inline ColaD<T>::ColaD(size_t s) : lmax_(s), elementos_(new T[s]), i_(0), n_(0)
+inline ColaD<T>::ColaD(int s) : lmax_(s), i_(0), n_(0), elementos_(new T[s])
 {
 
 }
 
+//Vacia
+template <typename T>
+inline bool ColaD<T>::vacia() const
+{
+    return(n_==0);
+}
 
+//Llena
+template <typename T>
+inline bool ColaD<T>::llena() const
+{
+    return(n_==lmax_);
+}
 
+//Tope
+template <typename T>
+const T& ColaD<T>::tope() const
+{
+    assert(!vacia());
+    return(elementos_[(i_+n_)%lmax_]);
+}
+
+//Push
+template <typename T>
+void ColaD<T>::push(const T& in)
+{
+    assert(!llena());
+    elementos_[(i_+n_)%lmax_]=in;
+    n_++;
+}
+
+//Pop
+template <typename T>
+T ColaD<T>::pop()
+{
+    assert(!vacia());
+    i_=(i_+1)%lmax_;
+    return(elementos_[(i_-1)%lmax_]);
+}
+
+//Destructor
+template <typename T>
+ColaD<T>::~ColaD()
+{
+    delete[] elementos_;
+}
 
 #endif // COLA_DINAMICA_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@ using namespace std;
 
 int main()
 {
-
+    ColaD<int> a(5);
+    a.push(getValue<int>());
+    a.push(getValue<int>());
+    a.push(getValue<int>());
+    a.push(getValue<int>());
+    cout << a.pop() << endl;
+    cout << a.pop() << endl;
+    cout << a.pop() << endl;
+    cout << a.pop() << endl;
+    cout << a.pop() << endl;
     return 0;
 }
