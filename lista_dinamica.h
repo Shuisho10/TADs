30443e4405930264d82f76a5925f65e9867383a7
Shuisho10 <54242777+Shuisho10@users.noreply.github.com>
2023-02-17 10:56:06 +0100

    Preparando lista dinamica

---
+++ lista_dinamica.h
@@ -0,0 +1,15 @@
+#ifndef LISTA_DINAMICA_H_INCLUDED
+#define LISTA_DINAMICA_H_INCLUDED
+
+template <typename T>
+struct ListaD
+{
+public:
+    ListaD();
+    ~ListaD();
+private:
+
+};
+
+
+#endif // LISTA_DINAMICA_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include "iotest.cpp"
-#include "cola_dinamica.h"
+#include "lista_dinamica.h"
 
 using namespace std;
 
 int main()
 {
-    ColaD<int> a(5);
-    a.push(getValue<int>());
-    a.push(getValue<int>());
-    a.push(getValue<int>());
-    a.push(getValue<int>());
-    cout << a.pop() << endl;
-    cout << a.pop() << endl;
-    cout << a.pop() << endl;
-    cout << a.pop() << endl;
-    cout << a.pop() << endl;
+
     return 0;
 }
